bc77862b570560115b3feb5c7641d3bedf85faa0
Julian Estefan <julian.estefan@hotovo.com>
2022-05-16 21:53:34 -0300

    Added database  config to connect in rpoduction

--- digital-wallet-backend/src/database/database.module.ts
+++ digital-wallet-backend/src/database/database.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { User } from 'src/user/user.entity';
 
-const options: TypeOrmModuleOptions = {
+const generalOptions: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT),
@@ -12,6 +12,17 @@ const options: TypeOrmModuleOptions = {
   synchronize: true,
   entities: [User],
 };
+
+const productionOptions = {
+  ssl: {
+      rejectUnauthorized: true
+  }
+}
+
+const options = process.env.NODE_ENV === 'production' ?
+  { ...generalOptions, ...productionOptions } :
+  generalOptions;
+
 
 @Module({ imports: [TypeOrmModule.forRoot(options)] })
 export class DatabaseModule {}
