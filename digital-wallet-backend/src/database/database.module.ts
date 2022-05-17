5aa20abf31f07b9e13b5787eb4099207c78e16ec
Julian Estefan <julian.estefan@hotovo.com>
2022-05-16 22:18:36 -0300

    Added databse url to manage production db conection

--- digital-wallet-backend/src/database/database.module.ts
+++ digital-wallet-backend/src/database/database.module.ts
@@ -2,19 +2,23 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { User } from 'src/user/user.entity';
 
-const ssl =
-  process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false;
+const connectionOptions: TypeOrmModuleOptions = process.env.DATABASE_URL
+  ? {
+      url: process.env.DATABASE_URL,
+    }
+  : {
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    };
 
 const options: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  ...connectionOptions,
   synchronize: true,
   entities: [User],
-  ssl
 };
 
 @Module({ imports: [TypeOrmModule.forRoot(options)] })
