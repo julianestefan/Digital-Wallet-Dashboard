2ce2aa02fe834e15cb058daec1227ad90fba1b95
Julian Estefan <julian.estefan@hotovo.com>
2022-05-16 22:52:20 -0300

    change in db config

--- digital-wallet-backend/src/database/database.module.ts
+++ digital-wallet-backend/src/database/database.module.ts
@@ -2,23 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { User } from 'src/user/user.entity';
 
-const connectionOptions: TypeOrmModuleOptions = process.env.DATABASE_URL
-  ? {
-      url: process.env.DATABASE_URL + '?sslmode=require',
-    }
-  : {
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    };
-
 const options: TypeOrmModuleOptions = {
-  ...connectionOptions,
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: true,
   entities: [User],
+  ssl: process.env.NODE_ENV === 'production'
 };
 
 console.log(options);
